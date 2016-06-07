a2a905d84efae975d56104fabfa6b513f17e5216
Lewis McReu <wimcreuwels@MacBook-Pro-van-Wim.local>
2016-06-07 17:26:01 +0200

    Update version number

--- src/main/java/com/minecraftuberverse/ubercore/Reference.java
+++ src/main/java/com/minecraftuberverse/ubercore/Reference.java
@@ -22,5 +22,5 @@ public class Reference
 {
 	public static final String MOD_ID = "ubercore";
 	public static final String MOD_NAME = "UberCore";
-	public static final String VERSION = "1.8-1.0";
+	public static final String VERSION = "1.8-alpha";
 }
